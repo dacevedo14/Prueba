4dea0038970a660d8876cb0d9b977686d6076edc
David Acevedo <dacevedo14@gmail.com>
2016-06-02 21:08:48 -0500

    Update

--- client.py
+++ client.py
@@ -1,48 +1,35 @@
 import requests
-
 import json
 
-
-
-
-
-class Client(Object):
-
+class Client:
 	def __init__(self, url):
-	
-	self.url = url
+		self.url = url
 
-
-
-	def concat_payload (self, volt, curr)
-		return {"Voltage":vol,"Current":curr}
-
-	def info_shows (self, response)
+	def info_shows (self, response):
 		info = response["payload"]
-		for array in info
+		for array in info:
 			print "ID:{}\nVoltaje:{}\nCorriente{}\n'----------'".format(array[0], array[1], array[2])
 		
 
 	def save(self, volt, curr):
-
 		header = {"Content-Type":"application/json" }
-
-		payload = concat_payload (volt, curr)
-    		url = "'url'+'/save'"
-
+		payload = {"Voltage":volt,"Current":curr}
+    		url = self.url + '/save'
     		r = requests.post(url = url, headers = header, data = json.dumps(payload))
     		response = r.json()
-		print "Status: %d" % response
+		
+		print response
 
+		if "status" in response.keys():
+			return response["status"]
+		else:
+			return None
 
 
 	def read(self, volt, curr):
-
     		header = {"Content-Type":"application/json" }
-
-		payload = concat_payload (volt, curr)
-    		url = "'url'+'/read'"
-
+		payload = {"Voltage":volt,"Current":curr}
+    		url = self.url + '/read' 
     		r = requests.post(url = url, headers = header, data = json.dumps(payload))
     		response = r.json()
 		info_shows (self, response)
@@ -50,12 +37,9 @@ class Client(Object):
 
 
 	def request(self, volt, curr):
-  %comparación
     		header = {"Content-Type":"application/json" }
-
-		payload = concat_payload (volt, curr)
-    		url = "'url'+'/request'"
-
+		payload = {"Voltage":"volt","Current":"curr"}
+    		url = self.url+'/request'
     		r = requests.post(url = url, headers = header, data = json.dumps(payload))    		
 		response = r.json()
 		info_shows (self, response)
