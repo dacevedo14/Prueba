6c31e64e6024cb7712cae05f9d6353da1bba63bd
David Acevedo <dacevedo14@gmail.com>
2016-06-08 17:56:13 -0500

    correcion request

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from json import dumps
 
 class Client:
 	def __init__(self, url):
@@ -30,26 +31,21 @@ class Client:
 
 		for key in db.keys():
 			print key,":",db[key]
-
+		print '-------------'
 
 	def request(self, volt, curr):
-		tv = type(volt)
-		tc = type (curr)
-		print tc
-		print tv	
-#		if tv = 'str' and tc = 'str':
+		
 		header = {"Content-Type":"application/json" }
 		payload = {"Voltage":volt,"Current":curr}
-    		url = self.url+'/request'
-		r = requests.post(url = url, headers = header, data = json.dumps(payload))    		
+		url = self.url+'/requests'
+		r = requests.post(url = url, headers = header, data = dumps(payload))    		
 		response = r.json()
 		db = response["payload"]
-		
+				
 		for data in db:
-			for key in data.keys:
-		Test.request("<2","<4")
-		print key,":", data[key]
-#		else:
-#			print('error en la variable')
+			for key in data.keys():
+				print key,":", data[key]
+			print '--------------------------'
+	
 
 			
